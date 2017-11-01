c4f6d8315f5032cd167e5b8fdc5fc7c54db670dd
Eric Marrone <emarrone17@gmail.com>
2017-11-01 15:30:45 -0400

    Add exercise 0

---
+++ exercises.rb
@@ -0,0 +1,12 @@
+# Exercise 0
+
+fav_colours = ["blue", "green", "red", "gray"]
+family_ages = [27, 32, 28, 25]
+coin_flips_heads = ["yes", "no", "yes", "no", "no"]
+fav_artists = ["slushii", "marshmello", "drake"]
+fav_colours_symbols = [:blue, :green, :red, :gray]
+
+definitions = {:pizza => "I like pizza", :book => "Something you read", :door => "you walk through it"}
+fav_movies = {:Austin_Powers => 1997, :transformers => 2007, :quantum_of_solace => 2008}
+cities = {:toronto => 2800000, :chicago => 2700000, :newyork => 8500000}
+siblings = {:chris => 32, :matt => 26, :josh =>28}
