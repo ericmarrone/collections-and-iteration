3fda6d51bcc019eda8e1fae8e0fa79bdaf73dcdb
Eric Marrone <emarrone17@gmail.com>
2017-11-01 15:50:20 -0400

    Add exercise 1

--- exercises.rb
+++ exercises.rb
@@ -2,7 +2,7 @@
 
 fav_colours = ["blue", "green", "red", "gray"]
 family_ages = [27, 32, 28, 25]
-coin_flips_heads = ["yes", "no", "yes", "no", "no"]
+coin_flips = ["heads", "tails", "heads", "heads", "tails"]
 fav_artists = ["slushii", "marshmello", "drake"]
 fav_colours_symbols = [:blue, :green, :red, :gray]
 
@@ -10,3 +10,11 @@ definitions = {:pizza => "I like pizza", :book => "Something you read", :door =>
 fav_movies = {:Austin_Powers => 1997, :transformers => 2007, :quantum_of_solace => 2008}
 cities = {:toronto => 2800000, :chicago => 2700000, :newyork => 8500000}
 siblings = {:chris => 32, :matt => 26, :josh =>28}
+
+# Exercise 1
+
+p coin_flips
+p fav_colours[0]
+p siblings.sort
+siblings[:baby] = 0
+p fav_movies[:transformers]
