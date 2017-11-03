e5753c7f7bee3f06a015a30532595257ed78326a
Eric Marrone <emarrone17@gmail.com>
2017-11-03 01:13:19 -0400

    Add exercise 3

--- exercises.rb
+++ exercises.rb
@@ -7,7 +7,7 @@ fav_artists = ["slushii", "marshmello", "drake"]
 fav_colours_symbols = [:blue, :green, :red, :gray]
 
 definitions = {:pizza => "I like pizza", :book => "Something you read", :door => "you walk through it"}
-fav_movies = {:Austin_Powers => 1997, :transformers => 2007, :quantum_of_solace => 2008}
+fav_movies = {"Austin Powers" => 1997, "Transformers" => 2007, "Quantum of Solace" => 2008}
 cities = {:toronto => 2800000, :chicago => 2700000, :newyork => 8500000}
 siblings = {:chris => 32, :matt => 26, :josh =>28}
 
@@ -28,3 +28,16 @@ p cities[:toronto]
 puts "I think #{fav_artists[0]} is great"
 puts "I think #{fav_artists[1]} is great"
 puts "I think #{fav_artists[2]} is great"
+
+# Exercise 3
+
+p fav_artists[0..1]
+puts fav_movies.keys[0] + " was released in #{fav_movies["Austin Powers"]}"
+puts fav_movies.keys[1] + " was released in #{fav_movies["Transformers"]}"
+puts fav_movies.keys[2] + " was released in #{fav_movies["Quantum of Solace"]}"
+
+family_sort_reverse = family_ages.sort.reverse
+p family_sort_reverse
+
+fav_movies["Beauty and the Beast"] = 1991, 2017
+p fav_movies
